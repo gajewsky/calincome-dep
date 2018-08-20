18deb13fac75674ca92a8dc8ca5bd4f871b6ed30
gajewsky <marcin.gajewski@rebased.pl>
2018-08-20 23:10:33 +0200

    Rename Gemfile, setup rubocop and slim-linter

---
+++ gems.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '2.5.1'
+
+gem 'bootsnap', '>= 1.1.0', require: false
+gem 'jbuilder', '~> 2.5'
+gem 'pg', '>= 0.18', '< 2.0'
+gem 'puma', '~> 3.11'
+gem 'rails', '~> 5.2.0'
+gem 'slim'
+gem 'slim-rails'
+gem 'webpacker'
+
+group :development, :test do
+  gem 'pry'
+  gem 'rubocop'
+  gem 'slim_lint'
+end
